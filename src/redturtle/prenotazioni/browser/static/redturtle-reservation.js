c243370d27c269355ec9a7ef763650fcca0ca879
Giulia Ghisini <giulia.ghisini@redturtle.it>
2020-10-28 14:22:13 +0100

    added style to book button

--- src/redturtle/prenotazioni/browser/static/redturtle-reservation.js
+++ src/redturtle/prenotazioni/browser/static/redturtle-reservation.js
@@ -1,37 +1,43 @@
-require([
-    'jquery',
-  ], function($) {
-    'use strict';
+require(["jquery"], function ($) {
+  "use strict";
 
-    // function rgdateinput() {
-    //     if (!$.fn.dateinput) {
-    //         return false;
-    //     }
+  // function rgdateinput() {
+  //     if (!$.fn.dateinput) {
+  //         return false;
+  //     }
 
-    //     function sync_inputs(backend, frontend) {
-    //         backend.val(frontend.getValue('yyyy-mm-dd'));
-    //     }
+  //     function sync_inputs(backend, frontend) {
+  //         backend.val(frontend.getValue('yyyy-mm-dd'));
+  //     }
 
-    //     $('.rg-dateinput').each(
-    //         function (idx, el) {
-    //             var frontend = jQuery(el).removeClass('rg-dateinput');
-    //             var backend = frontend.clone().attr({ type: 'hidden' });
-    //             jQuery('label[for="' + backend.attr('name') + '"] .formHelp').remove();
-    //             frontend.after(backend);
-    //             frontend.dateinput(
-    //                 {
-    //                     change: function () {
-    //                         sync_inputs(backend, this);
-    //                     }
-    //                 }
-    //             );
-    //             frontend.removeClass('date');
-    //             frontend.attr({ name: backend.attr('name') + Math.random() });
-    //         }
-    //     );
-    // }
-    // $(document).ready(rgdateinput);
-    $(document).on("afterdatagridfieldinit", function() {
-        $( "input[name$='widgets.day']" ).attr("readonly", "readonly");
-    });
+  //     $('.rg-dateinput').each(
+  //         function (idx, el) {
+  //             var frontend = jQuery(el).removeClass('rg-dateinput');
+  //             var backend = frontend.clone().attr({ type: 'hidden' });
+  //             jQuery('label[for="' + backend.attr('name') + '"] .formHelp').remove();
+  //             frontend.after(backend);
+  //             frontend.dateinput(
+  //                 {
+  //                     change: function () {
+  //                         sync_inputs(backend, this);
+  //                     }
+  //                 }
+  //             );
+  //             frontend.removeClass('date');
+  //             frontend.attr({ name: backend.attr('name') + Math.random() });
+  //         }
+  //     );
+  // }
+  // $(document).ready(rgdateinput);
+  $(document).on("afterdatagridfieldinit", function () {
+    $("input[name$='widgets.day']").attr("readonly", "readonly");
   });
+  $(document).ready(function () {
+    var actionBook = $("#form-buttons-action_book");
+    if (actionBook) {
+      console.log(actionBook);
+
+      actionBook.addClass("context");
+    }
+  });
+});
