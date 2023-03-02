fc05a85c07258c6f730e99377d669f1418f75433
Mauro Amico <mauro.amico@gmail.com>
2023-03-02 15:13:11 +0100

    flake8

--- setup.py
+++ setup.py
@@ -63,8 +63,8 @@ setup(
         "collective.z3cform.datagridfield>=2.0",
         "pyexcel_ods3",
         "click",
-# FIXME: se si rimuove il profilo di caching da qui (perchè c'è?), si può togliere anche questo pin
-# 3.0.0a14 e successive richiedono plone.base che è solo su plone 6
+        # FIXME: se si rimuove il profilo di caching da qui (perchè c'è?), si può togliere anche questo pin
+        # 3.0.0a14 e successive richiedono plone.base che è solo su plone 6
         "plone.app.caching>=3.0.0a1",
     ],
     extras_require={
