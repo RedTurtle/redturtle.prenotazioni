974ed20ee64cefca3946be7e0aca118753629497
Andrea Cecchi <andrea.cecchi85@gmail.com>
2023-04-07 12:47:06 +0200

    Add endpoint for month slots (#34)

---
+++ src/redturtle/prenotazioni/restapi/services/month_slots/get.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from plone import api
+from plone.restapi.serializer.converters import json_compatible
+from plone.restapi.services import Service
+
+import calendar
+
+
+class MonthSlots(Service):
+    def reply(self):
+        """
+        Finds all the available slots in a month.
+
+        If you pass a date in querystring, the search will start from that date during the whole month.
+        If not, the search will start from current date.
+        """
+
+        prenotazioni_week_view = api.content.get_view(
+            "prenotazioni_week_view", context=self.context, request=self.request
+        )
+        now = self.request.form.get("date", "")
+        if now:
+            now = date.fromisoformat(now)
+        else:
+            now = date.today()
+
+        start_year = now.year
+        start_month = now.month
+        start_day = now.day
+
+        response = {"@id": f"{self.context.absolute_url()}/@month_slots", "items": []}
+
+        for week in calendar.monthcalendar(start_year, start_month):
+            for day in week:
+                if day < start_day:
+                    continue
+                booking_date = date(start_year, start_month, day)
+                slots = prenotazioni_week_view.prenotazioni.get_anonymous_slots(
+                    booking_date=booking_date
+                )
+                for slot in slots.get("anonymous_gate", []):
+                    info = (
+                        prenotazioni_week_view.prenotazioni.get_anonymous_booking_url(
+                            booking_date, slot
+                        )
+                    )
+                    if not info.get("url", ""):
+                        continue
+                    response["items"].append(
+                        json_compatible(info.get("booking_date", ""))
+                    )
+        return response
---
+++ src/redturtle/prenotazioni/tests/test_month_slots.py
@@ -0,0 +1,241 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from plone import api
+from plone.app.testing import (
+    SITE_OWNER_NAME,
+    SITE_OWNER_PASSWORD,
+    TEST_USER_ID,
+    setRoles,
+)
+from plone.restapi.testing import RelativeSession
+from redturtle.prenotazioni.testing import REDTURTLE_PRENOTAZIONI_API_FUNCTIONAL_TESTING
+from datetime import datetime
+from plone.restapi.serializer.converters import json_compatible
+from redturtle.prenotazioni.adapters.booker import IBooker
+
+import calendar
+import unittest
+import transaction
+
+
+class TestMonthSlots(unittest.TestCase):
+    layer = REDTURTLE_PRENOTAZIONI_API_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.app = self.layer["app"]
+        self.portal = self.layer["portal"]
+        self.portal_url = self.portal.absolute_url()
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        self.api_session = RelativeSession(self.portal_url)
+        self.api_session.headers.update({"Accept": "application/json"})
+        self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+
+        self.folder_prenotazioni = api.content.create(
+            container=self.portal,
+            type="PrenotazioniFolder",
+            title="Prenota foo",
+            description="",
+            daData=date.today(),
+            week_table=[
+                {
+                    "day": "Lunedì",
+                    "morning_start": "0700",
+                    "morning_end": "1000",
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Martedì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Mercoledì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Giovedì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Venerdì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Sabato",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Domenica",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+            ],
+            booking_types=[
+                {"name": "Type A", "duration": "30"},
+            ],
+            gates=["Gate A"],
+        )
+
+        year = api.content.create(
+            container=self.folder_prenotazioni, type="PrenotazioniYear", title="Year"
+        )
+        week = api.content.create(container=year, type="PrenotazioniWeek", title="Week")
+        self.day_folder = api.content.create(
+            container=week, type="PrenotazioniDay", title="Day"
+        )
+        transaction.commit()
+
+    def test_month_slots_called_without_params_return_all_available_slots_of_current_month(
+        self,
+    ):
+        response = self.api_session.get(
+            "{}/@month-slots".format(self.folder_prenotazioni.absolute_url())
+        )
+        # get next mondays in current month
+        now = date.today()
+        current_year = now.year
+        current_month = now.month
+        current_day = now.day
+        expected = []
+        for week in calendar.monthcalendar(current_year, current_month):
+            # week[0] is monday and should be greater than today
+            if week[0] > current_day:
+                for hour in [7, 8, 9]:
+                    expected.append(
+                        json_compatible(
+                            datetime(current_year, current_month, week[0], hour, 0)
+                        )
+                    )
+
+        self.assertEqual(expected, response.json()["items"])
+
+    def test_month_slots_called_without_params_return_available_slots_of_current_month_when_some_are_full(
+        self,
+    ):
+
+        now = date.today()
+        current_year = now.year
+        current_month = now.month
+        current_day = now.day
+        monday = 0
+        # get next monday
+        for week in calendar.monthcalendar(current_year, current_month):
+            # week[0] is monday and should be greater than today
+            if week[0] > current_day:
+                monday = week[0]
+                break
+        # create a placeholder for first available monday
+        booker = IBooker(self.folder_prenotazioni)
+        booker.create(
+            {
+                "booking_date": datetime(current_year, current_month, monday, 7, 0),
+                "booking_type": "Type A",
+                "title": "foo",
+            }
+        )
+        transaction.commit()
+
+        # get next mondays in current month
+        now = date.today()
+        current_year = now.year
+        current_month = now.month
+
+        response = self.api_session.get(
+            "{}/@month-slots".format(self.folder_prenotazioni.absolute_url())
+        )
+
+        # first free slot is at 7:30
+        self.assertEqual(
+            response.json()["items"][0],
+            json_compatible(datetime(current_year, current_month, monday, 7, 30)),
+        )
+
+    def test_show_all_available_slots_for_next_month_from_the_beginning(
+        self,
+    ):
+        now = date.today()
+        current_year = now.year
+        current_month = now.month
+        next_month = current_month + 1
+
+        response = self.api_session.get(
+            "{}/@month-slots?date={}".format(
+                self.folder_prenotazioni.absolute_url(),
+                json_compatible(date(current_year, next_month, 1)),
+            )
+        )
+
+        # get next mondays in current month
+        expected = []
+        for week in calendar.monthcalendar(current_year, next_month):
+            monday = week[0]
+            if monday > 0:
+                for hour in [7, 8, 9]:
+                    expected.append(
+                        json_compatible(
+                            datetime(current_year, next_month, monday, hour, 0)
+                        )
+                    )
+        self.assertEqual(expected, response.json()["items"])
+
+    def test_month_slots_notBeforeDays_honored(
+        self,
+    ):
+        now = date.today()
+        week_table = []
+        for i in range(0, 7):
+            data = {
+                "day": str(i),
+                "morning_start": None,
+                "morning_end": None,
+                "afternoon_start": None,
+                "afternoon_end": None,
+            }
+            if i == now.weekday() + 1:
+                # open only for tomorrow
+                data["morning_start"] = "0700"
+                data["morning_end"] = "0800"
+            week_table.append(data)
+
+        folder = api.content.create(
+            container=self.portal,
+            type="PrenotazioniFolder",
+            title="Prenota foo",
+            description="",
+            daData=now,
+            week_table=week_table,
+            booking_types=[
+                {"name": "Type A", "duration": "30"},
+            ],
+            gates=["Gate A"],
+        )
+        transaction.commit()
+
+        response = self.api_session.get("{}/@month-slots".format(folder.absolute_url()))
+
+        tomorrow = json_compatible(datetime(now.year, now.month, now.day + 1, 7, 0))
+        self.assertNotIn(tomorrow, response.json()["items"])
+
+        folder.notBeforeDays = 0
+        transaction.commit()
+
+        response = self.api_session.get("{}/@month-slots".format(folder.absolute_url()))
+        self.assertIn(tomorrow, response.json()["items"])
