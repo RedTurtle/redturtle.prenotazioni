4f224fa3669e5da9bbfe163a1e88d303fb5470f4
Andrea Cecchi <andrea.cecchi85@gmail.com>
2023-04-07 10:43:39 +0200

    add endpoint for month slots

--- src/redturtle/prenotazioni/restapi/services/week_slots/get.py
+++ src/redturtle/prenotazioni/restapi/services/week_slots/get.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 from plone import api
 from plone.restapi.interfaces import ISerializeToJson
 from plone.restapi.services import Service
@@ -69,4 +70,7 @@ class WeekSlots(Service):
                     for item in value
                 ]
 
-        return response
+        return {
+            "@id": "{}/@week-slots".format(self.contenxt.abosolute_url()),
+            "items": response,
+        }
