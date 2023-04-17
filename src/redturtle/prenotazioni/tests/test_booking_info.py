4d5e23f8c7cfa24d875c69a628ba1c60828dc914
Daniele Andreotti <daniele.andreotti@redturtle.it>
2023-04-17 12:18:35 +0200

    added tests

---
+++ src/redturtle/prenotazioni/tests/test_booking_info.py
@@ -0,0 +1,177 @@
+# -*- coding: utf-8 -*-
+from datetime import date
+from plone import api
+from plone.app.testing import (
+    SITE_OWNER_NAME,
+    SITE_OWNER_PASSWORD,
+    TEST_USER_ID,
+    setRoles,
+)
+from plone.restapi.testing import RelativeSession
+from redturtle.prenotazioni.testing import REDTURTLE_PRENOTAZIONI_API_FUNCTIONAL_TESTING
+from datetime import datetime
+from plone.restapi.serializer.converters import json_compatible
+from redturtle.prenotazioni.adapters.booker import IBooker
+
+import calendar
+import unittest
+import transaction
+
+
+class TestBookingInfo(unittest.TestCase):
+    layer = REDTURTLE_PRENOTAZIONI_API_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.app = self.layer["app"]
+        self.portal = self.layer["portal"]
+        self.portal_url = self.portal.absolute_url()
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        self.api_session = RelativeSession(self.portal_url)
+        self.api_session.headers.update({"Accept": "application/json"})
+        self.api_session.auth = (SITE_OWNER_NAME, SITE_OWNER_PASSWORD)
+
+        self.folder_prenotazioni = api.content.create(
+            container=self.portal,
+            type="PrenotazioniFolder",
+            title="Prenota foo",
+            description="",
+            daData=date.today(),
+            week_table=[
+                {
+                    "day": "Lunedì",
+                    "morning_start": "0700",
+                    "morning_end": "1000",
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Martedì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Mercoledì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Giovedì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Venerdì",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Sabato",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+                {
+                    "day": "Domenica",
+                    "morning_start": None,
+                    "morning_end": None,
+                    "afternoon_start": None,
+                    "afternoon_end": None,
+                },
+            ],
+            booking_types=[
+                {"name": "Type A", "duration": "30"},
+            ],
+            gates=["Gate A"],
+            required_booking_fields=["email", "fiscalcode"],
+            visible_booking_field=["email", "fiscalcode"],
+        )
+
+        year = api.content.create(
+            container=self.folder_prenotazioni, type="PrenotazioniYear", title="Year"
+        )
+        week = api.content.create(container=year, type="PrenotazioniWeek", title="Week")
+        self.day_folder = api.content.create(
+            container=week, type="PrenotazioniDay", title="Day"
+        )
+        transaction.commit()
+
+    def test_create_booking_and_check_details(
+        self,
+    ):
+
+        now = date.today()
+        current_year = now.year
+        current_month = now.month
+        current_day = now.day
+        monday = 0
+        # get next monday
+        for week in calendar.monthcalendar(current_year, current_month):
+            # week[0] is monday and should be greater than today
+            if week[0] > current_day:
+                monday = week[0]
+                break
+        # create a placeholder for first available monday
+
+        booker = IBooker(self.folder_prenotazioni)
+        booking = booker.create(
+            {
+                "booking_date": datetime(current_year, current_month, monday, 7, 0),
+                "booking_type": "Type A",
+                "title": "foo",
+            }
+        )
+        transaction.commit()
+
+        booking_uid = booking.UID()
+
+        response = self.api_session.get(
+            "{}/@prenotazione/{}".format(
+                self.folder_prenotazioni.absolute_url(), booking_uid
+            )
+        )
+
+        self.assertIn("email", response.json())
+        self.assertIn("fiscalcode", response.json())
+
+    def test_get_booking_without_UID(
+        self,
+    ):
+
+        now = date.today()
+        current_year = now.year
+        current_month = now.month
+        current_day = now.day
+        monday = 0
+        # get next monday
+        for week in calendar.monthcalendar(current_year, current_month):
+            # week[0] is monday and should be greater than today
+            if week[0] > current_day:
+                monday = week[0]
+                break
+        # create a placeholder for first available monday
+
+        booker = IBooker(self.folder_prenotazioni)
+        booking = booker.create(
+            {
+                "booking_date": datetime(current_year, current_month, monday, 7, 0),
+                "booking_type": "Type A",
+                "title": "foo",
+            }
+        )
+        transaction.commit()
+
+        response = self.api_session.get(
+            "{}/@prenotazione".format(self.folder_prenotazioni.absolute_url())
+        )
+
+        self.assertEqual(404, response.status_code)
