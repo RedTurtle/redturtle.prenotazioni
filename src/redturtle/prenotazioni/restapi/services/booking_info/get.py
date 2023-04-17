cea05ba7ce0b6cd9b0ea249477ee051456129f0a
Daniele Andreotti <daniele.andreotti@redturtle.it>
2023-04-17 11:13:04 +0200

    using traverse logic

--- src/redturtle/prenotazioni/restapi/services/booking_info/get.py
+++ src/redturtle/prenotazioni/restapi/services/booking_info/get.py
@@ -4,14 +4,31 @@ from plone.restapi.interfaces import ISerializeToJson
 from plone.restapi.services import Service
 from zope.component import getMultiAdapter
 from AccessControl import Unauthorized
+from zope.publisher.interfaces import IPublishTraverse
+from zope.interface import implementer
 
 
+@implementer(IPublishTraverse)
 class BookingInfo(Service):
-    def reply(self):
-        catalog = api.portal.get_tool("portal_catalog")
 
-        query = {"portal_type": "Prenotazione", "UID": self.request.uid}
+    booking_uid = None
+
+    def publishTraverse(self, request, booking_uid):
+        if self.booking_uid is None:
+            self.booking_uid = booking_uid
+        return self
+
+    def reply(self):
+        if not self.booking_uid:
+            return self.reply_no_content(status=404)
+
+        catalog = api.portal.get_tool("portal_catalog")
+        query = {"portal_type": "Prenotazione", "UID": self.booking_uid}
         booking = catalog.unrestrictedSearchResults(query)
+
+        if not booking:
+            return self.reply_no_content(status=404)
+
         booking = booking[0]._unrestrictedGetObject()
 
         if api.user.is_anonymous():
