cf54e8e9874e76975eb4b3bde8075bc1332664af
Roman <72063601+folix-01@users.noreply.github.com>
2023-05-05 07:11:56 +0200

    add Endpoint that returns a list of *Prenotazione* content by parameters (#38)

--- CHANGES.rst
+++ CHANGES.rst
@@ -4,11 +4,16 @@ Changelog
 
 1.7.2 (unreleased)
 ------------------
-- Added a new endpoint to get booking details. (#40442).
+
+- Add @bookings endpoint to get booking items for a user
+  [foxtrot-dfm1]
+
+- Add a new endpoint to get booking details. (#40442).
   [daniele]
 
 - Add autoconfirm content rule to profile.
   [foxtrot-dfm1]
+
 - Added field "cosa_serve" (#40445).
   [daniele]
 
--- src/redturtle/prenotazioni/tests/test_booking_info.py
+++ src/redturtle/prenotazioni/tests/test_booking_info.py
@@ -1,20 +1,18 @@
 # -*- coding: utf-8 -*-
 from datetime import date
-from plone import api
-from plone.app.testing import (
-    SITE_OWNER_NAME,
-    SITE_OWNER_PASSWORD,
-    TEST_USER_ID,
-    setRoles,
-)
-from plone.restapi.testing import RelativeSession
-from redturtle.prenotazioni.testing import REDTURTLE_PRENOTAZIONI_API_FUNCTIONAL_TESTING
 from datetime import datetime
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import SITE_OWNER_NAME
+from plone.app.testing import SITE_OWNER_PASSWORD
+from plone.app.testing import TEST_USER_ID
+from plone.restapi.testing import RelativeSession
 from redturtle.prenotazioni.adapters.booker import IBooker
+from redturtle.prenotazioni.testing import REDTURTLE_PRENOTAZIONI_API_FUNCTIONAL_TESTING
 
 import calendar
-import unittest
 import transaction
+import unittest
 
 
 class TestBookingInfo(unittest.TestCase):
@@ -107,7 +105,6 @@ class TestBookingInfo(unittest.TestCase):
     def test_create_booking_and_check_details(
         self,
     ):
-
         now = date.today()
         current_year = now.year
         current_month = now.month
@@ -152,7 +149,6 @@ class TestBookingInfo(unittest.TestCase):
     def test_get_booking_without_UID(
         self,
     ):
-
         now = date.today()
         current_year = now.year
         current_month = now.month
