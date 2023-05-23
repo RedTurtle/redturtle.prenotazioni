149e456515f346f16f2f1469b0248c426551b7b6
Mauro Amico <mauro.amico@gmail.com>
2023-05-23 13:19:00 +0200

    spostati i js/css su una viewlet per semplificare compatibilità 5.2/6.0

--- src/redturtle/prenotazioni/browser/viewlets.py
+++ src/redturtle/prenotazioni/browser/viewlets.py
@@ -4,22 +4,44 @@ from plone.app.layout.viewlets.common import ViewletBase
 from zope.interface import Interface
 
 
-class IHasTableOverridesMarker(Interface):
-    """ """
-
-
-class HasTableOverridesWidget(ViewletBase):
-    """ """
+class HeadViewlet(ViewletBase):
+    """TEMP: workaround for compatibility plone 5.2/6.0"""
 
     def portal_url(self):
         return api.portal.get().absolute_url()
 
+    def hash(self):
+        return "0001"
+
     def render(self):
         return """
-<link href="{portal_url}/++plone++redturtle.prenotazioni/widget/dist/{mode}/main.css" rel="stylesheet"></link>
-<script src="{portal_url}/++plone++redturtle.prenotazioni/widget/dist/{mode}/main.js"></script>
+<link href="{portal_url}/++plone++redturtle.prenotazioni/redturtle-reservation.css?_={hash}" rel="stylesheet"></link>
+<script src="{portal_url}/++plone++redturtle.prenotazioni/redturtle-reservation.js?_={hash}"></script>
         """.format(
             portal_url=self.portal_url(),
-            # mode="prod",
+            hash=self.hash(),
+        )
+
+
+class IHasTableOverridesMarker(Interface):
+    """marker interface"""
+
+
+class HasTableOverridesWidget(HeadViewlet):
+    """TEMP: workaround for compatibility plone 5.2/6.0"""
+
+    def portal_url(self):
+        return api.portal.get().absolute_url()
+
+    def hash(self):
+        return "0001"
+
+    def render(self):
+        return """
+<link href="{portal_url}/++plone++redturtle.prenotazioni/widget/dist/{mode}/main.css?_={hash}" rel="stylesheet"></link>
+<script src="{portal_url}/++plone++redturtle.prenotazioni/widget/dist/{mode}/main.js?_={hash}"></script>
+        """.format(
+            portal_url=self.portal_url(),
             mode="dev" if api.env.debug_mode() else "prod",
+            hash=self.hash(),
         )
