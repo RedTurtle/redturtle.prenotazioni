269af82103e76bb86db8702bb500b0c4d3ada922
Mauro Amico <mauro.amico@gmail.com>
2023-07-20 09:21:44 +0200

    search bookings in folder (#50)

--- src/redturtle/prenotazioni/restapi/services/bookings/search.py
+++ src/redturtle/prenotazioni/restapi/services/bookings/search.py
@@ -20,31 +20,30 @@ class BookingsSearch(Service):
             request.set("userid", userid)
         return self
 
-    def reply(self):
-        start_date = self.request.get("from", None)
-        end_date = self.request.get("to", None)
-
+    def query(self):
+        query = {
+            "portal_type": "Prenotazione",
+        }
         if api.user.is_anonymous():
             raise Unauthorized("You must be logged in to perform this action")
         elif api.user.has_permission("redturtle.prenotazioni: search prenotazioni"):
             userid = self.request.get("userid", None)
         else:
             userid = api.user.get_current().getUserId()
-
-        response = {"id": self.context.absolute_url() + "/@bookings"}
-        query = {
-            "portal_type": "Prenotazione",
-        }
-
+        if userid:
+            query["fiscalcode"] = userid.upper()
+        start_date = self.request.get("from", None)
+        end_date = self.request.get("to", None)
         if start_date or end_date:
             query["Date"] = {
                 "query": [DateTime(i) for i in [start_date, end_date] if i],
                 "range": f"{start_date and 'min' or ''}{start_date and end_date and ':' or ''}{end_date and 'max' or ''}",  # noqa: E501
             }
+        return query
 
-        if userid:
-            query["fiscalcode"] = userid.upper()
-
+    def reply(self):
+        response = {"id": self.context.absolute_url() + "/@bookings"}
+        query = self.query()
         response["items"] = [
             getMultiAdapter(
                 (i.getObject(), self.request),
@@ -55,3 +54,10 @@ class BookingsSearch(Service):
         response["items_total"] = len(response["items"])
 
         return response
+
+
+class BookingsSearchFolder(BookingsSearch):
+    def query(self):
+        query = super(BookingsSearchFolder, self).query()
+        query["path"] = "/".join(self.context.getPhysicalPath())
+        return query
