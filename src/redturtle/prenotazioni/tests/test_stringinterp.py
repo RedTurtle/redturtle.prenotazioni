1c87618c2cea2d840c56c4445bd7096b9098ce78
Mauro Amico <mauro.amico@gmail.com>
2023-08-10 13:18:58 +0200

    fix date email + test

---
+++ src/redturtle/prenotazioni/tests/test_stringinterp.py
@@ -0,0 +1,85 @@
+# -*- coding: UTF-8 -*-
+from datetime import date
+from datetime import datetime
+from datetime import timedelta
+from plone import api
+from plone.app.testing import setRoles
+from plone.app.testing import TEST_USER_ID
+from plone.stringinterp.interfaces import IStringSubstitution
+from redturtle.prenotazioni.adapters.booker import IBooker
+from redturtle.prenotazioni.testing import REDTURTLE_PRENOTAZIONI_FUNCTIONAL_TESTING
+from zope.component import getAdapter
+
+import pytz
+import unittest
+
+
+class TestStringInterp(unittest.TestCase):
+    layer = REDTURTLE_PRENOTAZIONI_FUNCTIONAL_TESTING
+    maxDiff = None
+    timezone = "Europe/Rome"
+
+    def dt_local_to_utc(self, value):
+        return pytz.timezone(self.timezone).localize(value).astimezone(pytz.utc)
+
+    def setUp(self):
+        self.app = self.layer["app"]
+        self.portal = self.layer["portal"]
+        self.today_8_0 = self.dt_local_to_utc(
+            datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
+        )
+        self.tomorrow_8_0 = self.today_8_0 + timedelta(1)
+
+        setRoles(self.portal, TEST_USER_ID, ["Manager"])
+
+        self.folder_prenotazioni = api.content.create(
+            container=self.portal,
+            type="PrenotazioniFolder",
+            title="Prenota foo",
+            description="",
+            daData=date.today(),
+            booking_types=[
+                {"name": "Type A", "duration": "30"},
+            ],
+            gates=["Gate A"],
+        )
+        week_table = self.folder_prenotazioni.week_table
+        for data in week_table:
+            data["morning_start"] = "0700"
+            data["morning_end"] = "1000"
+        self.folder_prenotazioni.week_table = week_table
+
+        api.portal.set_registry_record(
+            "plone.portal_timezone",
+            self.timezone,
+        )
+
+    def test_stringinterp(self):
+        booker = IBooker(self.folder_prenotazioni)
+        booking = booker.create(
+            {
+                "booking_date": self.tomorrow_8_0,
+                "booking_type": "Type A",
+                "title": "foo",
+                "email": "jdoe@redturtle.it",
+            }
+        )
+        booking_print_url = getAdapter(
+            booking, IStringSubstitution, "booking_print_url"
+        )()
+        self.assertEqual(
+            booking_print_url,
+            f"http://nohost/plone/prenota-foo/@@prenotazione_print?uid={booking.UID()}",
+        )
+
+        # plone localized date
+        # booking_date = getAdapter(booking, IStringSubstitution, "booking_date")()
+        # booking_end_date = getAdapter(booking, IStringSubstitution, "booking_end_date")()
+
+        # localized time
+        booking_time = getAdapter(booking, IStringSubstitution, "booking_time")()
+        booking_time_end = getAdapter(
+            booking, IStringSubstitution, "booking_time_end"
+        )()
+        self.assertEqual(booking_time, "08:00")
+        self.assertEqual(booking_time_end, "08:30")
---
+++ src/redturtle/prenotazioni/tests/test_utils.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+from redturtle.prenotazioni import datetime_with_tz
+from redturtle.prenotazioni.testing import REDTURTLE_PRENOTAZIONI_API_FUNCTIONAL_TESTING
+
+import pytz
+import unittest
+
+
+class TestUtils(unittest.TestCase):
+    layer = REDTURTLE_PRENOTAZIONI_API_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.app = self.layer["app"]
+        self.portal = self.layer["portal"]
+        self.portal_url = self.portal.absolute_url()
+
+    def tearDown(self):
+        pass
+
+    def test_dateutils(self):
+        self.assertEqual(
+            datetime_with_tz("2023-08-09T13:30:00.000Z")
+            .astimezone(pytz.utc)
+            .isoformat(),
+            "2023-08-09T13:30:00+00:00",
+        )
+        self.assertEqual(
+            datetime_with_tz("2023-08-09T11:30:00.000+02")
+            .astimezone(pytz.utc)
+            .isoformat(),
+            "2023-08-09T09:30:00+00:00",
+        )
