6f5e4a8716d8e3241028e63fc440c0afe9c9db1a
Roman <roman.kysil@redturtle.it>
2023-08-17 12:00:36 +0200

    Fix mock

--- src/redturtle/prenotazioni/utils/migration.py
+++ src/redturtle/prenotazioni/utils/migration.py
@@ -1,7 +1,11 @@
 from zope.globalrequest import getRequest
-from collective.exportimport.interfaces import IMigrationMarker
+
+try:
+    from collective.exportimport.interfaces import IMigrationMarker
+except ImportError:
+    IMigrationMarker = None
 
 
 def is_migration():
     """Returns True if the current reqeust provides the migration marker"""
-    return IMigrationMarker.providedBy(getRequest())
+    return IMigrationMarker and IMigrationMarker.providedBy(getRequest())
