7cfee6929eca42a929aabfa930add89b598b2d33
Roman <roman.kysil@redturtle.it>
2023-10-30 16:14:26 +0100

    Add bookign_types to PrenotazioniFolder serialized to JSON

---
+++ src/redturtle/prenotazioni/restapi/serializers/adapters/prenotazioni_folder.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+from plone.restapi.interfaces import ISerializeToJson
+from plone.restapi.serializer.dxcontent import SerializeFolderToJson
+from zope.component import adapter, getMultiAdapter
+from zope.interface import implementer
+
+from redturtle.prenotazioni.content.prenotazioni_folder import IPrenotazioniFolder
+from redturtle.prenotazioni.interfaces import (
+    IRedturtlePrenotazioniLayer,
+    ISerializeToRetroCompatibleJson,
+)
+
+
+@implementer(ISerializeToJson)
+@adapter(IPrenotazioniFolder, IRedturtlePrenotazioniLayer)
+class PrenotazioniFolderSerializer(SerializeFolderToJson):
+    def __call__(self, *args, **kwargs):
+        res = super().__call__()
+
+        res["booking_types"] = [
+            getMultiAdapter((i, self.request), ISerializeToRetroCompatibleJson)()
+            for i in self.context.get_booking_types()
+        ]
+
+        return res
