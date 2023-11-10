5ad99b24eb14fc02f686b449fa0b6291946df55e
Mauro Amico <mauro.amico@gmail.com>
2023-11-10 13:16:05 +0100

    Fix sub BaseSlots whene some slots overlap

--- src/redturtle/prenotazioni/tests/test_base_slot.py
+++ src/redturtle/prenotazioni/tests/test_base_slot.py
@@ -12,19 +12,41 @@ class TestBaseSlot(unittest.TestCase):
     def test_sub(self):
         now = datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
         a = [
-            BaseSlot(now, now + timedelta(minutes=30)), 
+            BaseSlot(now, now + timedelta(minutes=30)),
             BaseSlot(now + timedelta(minutes=30), now + timedelta(hours=1)),
         ]
         b = BaseSlot(now, now + timedelta(hours=2))
 
-        self.assertEqual(b - a, [BaseSlot(now + timedelta(hours=1), now + timedelta(hours=2))])
+        self.assertEqual(
+            b - a, [BaseSlot(now + timedelta(hours=1), now + timedelta(hours=2))]
+        )
+
+    def test_sub_with_holes(self):
+        now = datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
+        a = [
+            BaseSlot(now, now + timedelta(minutes=30)),
+            BaseSlot(now + timedelta(minutes=75), now + timedelta(minutes=90)),
+            BaseSlot(now + timedelta(minutes=105), now + timedelta(minutes=110)),
+        ]
+        b = BaseSlot(now + timedelta(minutes=60), now + timedelta(minutes=120))
+
+        self.assertEqual(
+            b - a,
+            [
+                BaseSlot(now + timedelta(minutes=60), now + timedelta(minutes=75)),
+                BaseSlot(now + timedelta(minutes=90), now + timedelta(minutes=105)),
+                BaseSlot(now + timedelta(minutes=110), now + timedelta(minutes=120)),
+            ],
+        )
 
     def test_sub_overlapped(self):
         now = datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
         a = [
-            BaseSlot(now, now + timedelta(hours=1)), 
+            BaseSlot(now, now + timedelta(hours=1)),
             BaseSlot(now + timedelta(minutes=30), now + timedelta(hours=1)),
         ]
         b = BaseSlot(now, now + timedelta(hours=2))
 
-        self.assertEqual(b - a, [BaseSlot(now + timedelta(hours=1), now + timedelta(hours=2))])
+        self.assertEqual(
+            b - a, [BaseSlot(now + timedelta(hours=1), now + timedelta(hours=2))]
+        )
