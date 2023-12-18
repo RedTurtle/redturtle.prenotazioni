9919fa55ec2977e249db78d3bf63e5d2a513a4e2
Roman <roman.kysil@redturtle.it>
2023-12-18 14:18:46 +0100

    Fix notifications supervisor logics

--- src/redturtle/prenotazioni/behaviors/booking_folder/__init__.py
+++ src/redturtle/prenotazioni/behaviors/booking_folder/__init__.py
@@ -30,9 +30,7 @@ class BookingNotificationSupervisorUtility:
         ):
             return False
 
-        fiscalcode = booking.fiscalcode
-
-        if not fiscalcode:
+        if not booking.fiscalcode:
             return False
 
         if self._check_user_appio_subscription_to_booking_type(booking):
@@ -43,22 +41,26 @@ class BookingNotificationSupervisorUtility:
     def is_sms_message_allowed(self, booking):
         if not getattr(
             booking.getPrenotazioniFolder(), "notifications_sms_enabled", False
-        ) and getattr(booking, "email", None):
+        ):
             return False
 
         if self.is_email_message_allowed(booking):
             return False
 
-        if not self.is_appio_message_allowed(booking):
-            if booking.phone:
-                return True
+        if self.is_appio_message_allowed(booking):
+            return False
+
+        if booking.phone:
+            True
 
         return False
 
     def _check_user_appio_subscription_to_booking_type(self, booking):
         service_code = getattr(booking.get_booking_type(), "service_code", "")
+
         if not service_code:
             return False
+
         return Api(secret=os.environ.get(service_code)).is_service_activated(
             booking.fiscalcode
         )
