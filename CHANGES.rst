1dc37dc7867d49ac888dddfafce0e343101036a8
Mauro Amico <mauro.amico@gmail.com>
2024-01-03 23:07:49 +0100

    remove unused content rules

--- CHANGES.rst
+++ CHANGES.rst
@@ -5,6 +5,9 @@ Changelog
 2.4.0 (unreleased)
 ------------------
 
+- remove unused contentrules
+  [mamico]
+
 - Split booking notification gateways to 'App IO', 'Email' and 'SMS'
   [folix-01]
 
--- src/redturtle/prenotazioni/contentrules/handlers.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from plone.app.contentrules.handlers import execute_rules
-
-
-def moved(event):
-    """
-    When a booking is moved, execute the rules assigned to his parent.
-    """
-    execute_rules(event)
