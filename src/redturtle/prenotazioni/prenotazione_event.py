58bbacbdcea229aa7c9cd9d86788b77f5edfc958
Roman <roman.kysil@redturtle.it>
2024-01-09 14:53:26 +0100

    Inherit Booking moved event from the ObjectModifiedEvent

--- src/redturtle/prenotazioni/prenotazione_event.py
+++ src/redturtle/prenotazioni/prenotazione_event.py
@@ -1,16 +1,16 @@
 # -*- coding: utf-8 -*-
 from zope.interface import implementer
-from zope.interface.interfaces import IObjectEvent
-from zope.interface.interfaces import ObjectEvent
+from zope.lifecycleevent import ObjectModifiedEvent
+from zope.lifecycleevent.interfaces import IObjectModifiedEvent
 
 
-class IMovedPrenotazione(IObjectEvent):
+class IMovedPrenotazione(IObjectModifiedEvent):
 
     """Marker interface for prenotazione that is moved"""
 
 
 @implementer(IMovedPrenotazione)
-class MovedPrenotazione(ObjectEvent):
+class MovedPrenotazione(ObjectModifiedEvent):
 
     """Event fired when a prenotazione that is moved"""
 
