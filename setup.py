4e1b83151f838926d65ce34b94921e19a0b266d7
Mauro Amico <mauro.amico@gmail.com>
2024-02-29 10:15:15 +0100

    Back to development: 2.5.1

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ long_description = "\n\n".join(
 
 setup(
     name="redturtle.prenotazioni",
-    version="2.5.0",
+    version="2.5.1.dev0",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
