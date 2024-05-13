d6abb21d4cb75b52a7de91a47d0997bb322013c5
Roman <roman.kysil@redturtle.it>
2024-05-13 10:11:03 +0200

    Isort

--- src/redturtle/prenotazioni/behaviors/booking_folder/notifications/__init__.py
+++ src/redturtle/prenotazioni/behaviors/booking_folder/notifications/__init__.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 from functools import wraps
 
-from Products.CMFCore.utils import getToolByName
 from plone import api
+from Products.CMFCore.utils import getToolByName
 
 from redturtle.prenotazioni import _
 
