38a733fbf5346fdb6228249482916607a449d470
Roman Kysil <roman.kysil@redturtle.it>
2025-02-24 14:38:14 +0100

    Back to development: 2.8.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ long_description = "\n\n".join(
 
 setup(
     name="redturtle.prenotazioni",
-    version="2.8.7",
+    version="2.8.8.dev0",
     description="An add-on for Plone",
     long_description=long_description,
     # Get more from https://pypi.python.org/pypi?%3Aaction=list_classifiers
