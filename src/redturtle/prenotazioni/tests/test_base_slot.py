5d2fcf3ea8dc380520e6cb2e83f49cce60bd3945
Mauro Amico <mauro.amico@gmail.com>
2025-05-08 17:50:50 +0200

    test overlap intervals

--- src/redturtle/prenotazioni/tests/test_base_slot.py
+++ src/redturtle/prenotazioni/tests/test_base_slot.py
@@ -52,3 +52,34 @@ class TestBaseSlot(unittest.TestCase):
         self.assertEqual(
             b - a, [BaseSlot(now + timedelta(hours=1), now + timedelta(hours=2))]
         )
+
+    def test_overlaps(self):
+        now = datetime.now().replace(hour=8, minute=0, second=0, microsecond=0)
+        slot = BaseSlot(now, now + timedelta(hours=1))
+        self.assertTrue(slot.overlaps(BaseSlot(now, now + timedelta(hours=1))))
+        self.assertTrue(
+            slot.overlaps(
+                BaseSlot(now - timedelta(minutes=10), now + timedelta(minutes=10))
+            )
+        )
+        self.assertTrue(
+            slot.overlaps(
+                BaseSlot(now + timedelta(minutes=10), now + timedelta(minutes=30))
+            )
+        )
+        self.assertTrue(
+            slot.overlaps(
+                BaseSlot(now + timedelta(minutes=50), now + timedelta(minutes=70))
+            )
+        )
+
+        self.assertFalse(
+            slot.overlaps(
+                BaseSlot(now - timedelta(minutes=20), now - timedelta(minutes=10))
+            )
+        )
+        self.assertFalse(
+            slot.overlaps(
+                BaseSlot(now + timedelta(minutes=61), now - timedelta(minutes=70))
+            )
+        )
