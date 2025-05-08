d14e5c736296e476e6a98ccb15eed3b6109dd159
Mauro Amico <mauro.amico@gmail.com>
2025-05-08 17:55:56 +0200

    removed unused robotframework/fix requirements

--- setup.py
+++ setup.py
@@ -77,7 +77,7 @@ setup(
             # Remove if your package shall be part of coredev.
             # plone_coredev tests as of 2016-04-01.
             "plone.testing>=5.0.0",
-            "plone.app.contenttypes",
+            "plone.app.contenttypes[test]",
             "collective.MockMailHost",
             "freezegun",
         ],
