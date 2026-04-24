79119f5079574f507567059737717ec6463bdc74
Mauro Amico <mauro.amico@gmail.com>
2026-04-24 13:53:21 +0200

    aggiunti tempi lunghi sul range

--- src/redturtle/prenotazioni/vocabularies/voc_durata_incontro.py
+++ src/redturtle/prenotazioni/vocabularies/voc_durata_incontro.py
@@ -10,18 +10,25 @@ from zope.schema.vocabulary import SimpleTerm
 from zope.schema.vocabulary import SimpleVocabulary
 
 
-class VocabItem(object):
+class VocabItem:
     def __init__(self, token, value):
         self.token = token
         self.value = value
 
 
 @implementer(IVocabularyFactory)
-class VocDurataIncontro(object):
-    """ """
+class VocDurataIncontro:
+    """ La durata dell'incontro è espressa in minuti, e può essere compresa tra
+        5 minuti e 12 ore (720 minuti).
+        La durata deve essere un multiplo di 5 minuti fino a 3 ore (180 minuti), e
+        un multiplo di 30 minuti da 3 ore a 12 ore.
+    """
 
     def __call__(self, context):
-        items = [VocabItem(x, x) for x in range(5, 185, 5)]
+        items = (
+            [VocabItem(x, x) for x in range(5, 185, 5)] +
+            [VocabItem(x, x) for x in range(210, 12*60, 30)]
+        )
 
         if not IDexterityContent.providedBy(context):
             req = getRequest()
